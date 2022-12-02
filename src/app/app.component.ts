f639259fa66632ab359d6e75b48de648ce2d3185
Bryan Arroyave <bryan.arroyave@coxti.net>
2022-12-01 19:44:56 -0500

    TYPE: FEAT
    add app container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable no-useless-constructor */
 import { Component, OnInit } from '@angular/core';
+import IFligthResponse from './models/IFligthResponse';
+import { AirlineService } from './services/airline.service';
 import { ConsumeService } from './services/consumer.service';
 
 @Component({
@@ -9,13 +13,22 @@ import { ConsumeService } from './services/consumer.service';
 export class AppComponent implements OnInit {
   title = 'newshore_air';
 
-  constructor(private readonly consumerService: ConsumeService) {
-
-  }
+  constructor(
+    private readonly consumerService: ConsumeService,
+    private readonly airlineService: AirlineService
+  ) { }
 
   ngOnInit(): void {
-    this.consumerService.getFlights().subscribe((res: any) => {
-      console.log(res);
+    this.consumerService.getFlights().subscribe((res: IFligthResponse[]) => {
+      this.airlineService.init(res);
     });
+  }
+
+  getPath(origin: string, destination: string, cant: number) {
+    this.airlineService.getPath(origin, destination, cant);
+  }
+
+  sendData({ origin, destination, cant }: any) {
+    this.getPath(origin, destination, cant);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { UppercaseDirective } from './directives/uppercase.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    UppercaseDirective
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    MatMenuModule,
+    MatSlideToggleModule,
+    MatStepperModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
